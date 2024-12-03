a39b8ea3c8d7acd3457e3e695da665ccf4a3e67f
Melanie Bauer <me.bauer2008@gmail.com>
2024-12-03 14:03:19 +0100

    added code for day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("TestInput.txt")
+            .Select(line => line.Split("   "))
+            .Select(parts => (Left: int.Parse(parts[0]), Right: int.Parse(parts[1])))
+            .ToList();
+
+var silverStarSum = input
+    .OrderBy(x => x.Left).Zip(input.OrderBy(x => x.Right),
+        (l, r) => Math.Abs(l.Left - r.Right))
+    .Sum();
+
+var goldStarSum = input
+    .Select(x => x.Left * input.Count(y => y.Right == x.Left))
+    .Sum();
+
+Console.WriteLine($"Silver Star: {silverStarSum}");
+Console.WriteLine($"Gold Star: {goldStarSum}");
